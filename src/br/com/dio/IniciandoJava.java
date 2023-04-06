88fbd0777bc7b65b0c31139230e21f15756dabfb
origin <renan.victor325@outlook.com>
2023-04-06 10:18:12 -0300

    first commit

---
+++ src/br/com/dio/IniciandoJava.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class IniciandoJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! "  + (a+b));*/
+    }
+}
