e8af350d768473dc28973f270609c6e5b0ce6a9c
origin <renan.victor325@outlook.com>
2023-04-24 20:16:22 -0300

    #primeirasemana

---
+++ src/edu/renan/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,12 @@
+package edu.renan.primeirasemana;
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if (mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova de Recuperação");
+        else
+            System.out.println("Aprovado, Parabénsss!!!!!!");
+    }
+}
